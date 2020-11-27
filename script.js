a42684d6338ba306556833cbe4100d2bb2d03bf1
sameer <techallyoublog@gmail.com>
2020-11-27 18:07:55 +0530

    complete challage 1

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function ageInDays(){
-    let birthYear = prompt('what year you bron?');
-    console.log(birthYear)
+    let age = prompt('what is your age');    
+    let count = age * 365;
+    let final = "your age is --> " + age + '\n' + "you are "+  count + " days olds";
+
+    document.getElementById('answer').innerHTML = final;
+    
+}
+
+function reste(){
+    
+    document.getElementById('answer').innerHTML = '';
+
 }
 
